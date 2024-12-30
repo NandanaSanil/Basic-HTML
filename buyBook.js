88d43e2dc14df6946cc936cd35d5c3b1931178b4
NandanaSanil <nandanasanil02@gmail.com>
2024-12-30 20:30:10 +0530

    first commit

---
+++ buyBook.js
@@ -0,0 +1,15 @@
+
+const buyNowButton = document.getElementById('buy-now');
+
+
+buyNowButton.addEventListener('click', function() {
+  
+  window.location.href = 'image-page.html';
+});
+
+
+// const imageUrl = 'book-cover.jpg'; // or 'images/book-cover.jpg' or 'https://example.com/images/book-cover.jpg'
+// document.getElementById('book-image').src = imageUrl;
+
+// const google ImageUrl = 'https://drive.google.com/uc?export=view&id=IMAGE_ID';
+// document.getElementById('google-image').src = googleImageUrl;
---
+++ demo.js
@@ -0,0 +1,6 @@
+var a =1
+var ab=3
+let abd =6
+const close =8
+
+console.log(window);
